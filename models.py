3c23fb4cb39b4970ca89d5e803d865dc25ba9f9d
Kolganat <kolganatnauryzulu97@gmail.com>
2026-03-29 16:39:25 +0500

    .

---
+++ models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Account(db.MODEL):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    name = db.Column(db.String(120))
+    surname = db.Column(db.String(120))
+    phone = db.Column(db.String(20))
+    city = db.Column(db.String(120))
+    region = db.Column(db.String(120))
+
+class StuDate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
+    resume = db.Column(db.String(255))
+    video = db.Column(db.String(255))
+    test = db.Column(db.String(255))
+    essay = db.Column(db.String(255))
+    unt = db.Column(db.String(255))
+    ai_rate = db.Column(db.Integer)
+    rate_comittee = db.Column(db.Integer)
+    fin_rate = db.Column(db.Integer)
+    ai_report = db.Column(db.Text)
